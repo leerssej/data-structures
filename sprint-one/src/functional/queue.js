32304179aef2fa951047c7bc6f4d8e2d0b175ac9
leerssej <jens.leerssen@gmail.com>
2018-06-13 15:49:06 -0700

    makes Functional tests pass scrubbed, refactored, better variable names

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,22 +5,15 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
-    console.log(...Object.keys(storage));
     let nextIn = Math.max(-1, ...(Object.keys(storage))) + 1;
     storage[nextIn] = value;
-    console.log(storage);
   };
 
   someInstance.dequeue = function() {
-    console.log(...Object.keys(storage));
     let nextOut = Math.min(...(Object.keys(storage)));
-    console.log(nextOut);
     let nextValueOut = storage[nextOut];
-    console.log('storageBefore: ', storage, nextValueOut);
     delete storage[nextOut];
-    console.log('storageAfter: ', storage);
     return nextValueOut;
   };
 
@@ -30,11 +23,3 @@ var Queue = function() {
 
   return someInstance;
 };
-
-// let queue = Queue();
-// console.log(queue.enqueue("cow"));
-// console.log(queue.enqueue("cat"));
-// console.log(queue.enqueue("buffalo"));
-// console.log(queue.size());
-// console.log(queue.dequeue());
-// console.log(queue.dequeue());
