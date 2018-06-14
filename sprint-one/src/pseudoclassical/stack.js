4e21e16c9b05184aafbce6f078b61be2045f3536
leerssej <jens.leerssen@gmail.com>
2018-06-13 22:32:29 -0700

    pseudoclassical Stack OOLStorage Structs tests pass local this.storage in function stackMethods function stubs created

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,12 @@
-var Stack = function() {
+var Stack = function () {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  
 };
 
+Stack.prototype.size = function () { };
 
+Stack.prototype.push = function () { };
+
+Stack.prototype.pop = function () { };
