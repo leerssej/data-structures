c3dbabd361991be993f9f447062b9676ce1d2847
leerssej <jens.leerssen@gmail.com>
2018-06-13 23:08:32 -0700

    protoypal Queue OOLikeStruct passes tests create someInstance alias with Object.create to queueMethods Queue object method assignment stubs added in value parameter added to Stack and Queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,20 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+
+  return someInstance;
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {},
+
+  enqueue: function(value) {},
+
+  dequeue: function() {},
+
+};
 
 
