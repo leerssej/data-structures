a68df7bef52df9b8bd8bbad3173b76c4ff44c163
leerssej <jens.leerssen@gmail.com>
2018-06-13 22:31:30 -0700

    prototypal Stack OOLStorage Structs tests pass aliasing stackMethods to someInstance Object (Object.create) adding storage to someInstance stackMethods function stubs created

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,18 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+
+  return someInstance;
+
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {},
 
+  push: function() {},
 
+  pop: function() {},
+
+};
