963b176d6dd3a7608b1c1654aa2ebbf7bf51b2ff
leerssej <jens.leerssen@gmail.com>
2018-06-13 21:25:43 -0700

    funcShared Stack push and pop added

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -4,7 +4,7 @@ var extend = function (obj, methods) {
   }
 };
 
-var Stack = function() {
+var Stack = function(value) {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {
@@ -19,10 +19,18 @@ var Stack = function() {
 };
 
 var stackMethods = {
+
   push: function(value) {
+    this.storage[Object.keys(this.storage).length] = value;
   },
+
   pop: function() {
+    let nextOut = Math.max(...(Object.keys(this.storage)));
+    let nextValueOut = this.storage[nextOut];
+    delete this.storage[nextOut];
+    return nextValueOut;
   },
+
   size: function() {
     return Object.keys(this.storage).length;
   },
