c0517e2e6fcb5ebcc06409ef1f75f21eef22e80d
leerssej <jens.leerssen@gmail.com>
2018-06-13 22:54:01 -0700

    es6 Queue OOLikeStruct passes tests self reference this.storage inside Queues Constructor simple methods stubs added in

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,13 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
+
+  size() {}
+
+  enqueue() {}
+  
+  dequeue() {}
 
 }
