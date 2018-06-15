aaf3f50a4d02f4012dcd4a0ea8515e808b840d9d
Hack Reactor Student <student@hackreactor.com>
2018-06-15 15:52:16 -0700

    Revised Hashtable insert and retrieve methods

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,24 +3,43 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  // for (let i = 0; i < this._limit; i++) {
+  //   this.storage.set(i, []);
+  // }
+  // console.log(this._storage.get(0))
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, [k, v]);
+  var index = getIndexBelowMaxForKey(k, this._limit);  
+  var tempArrToModify = this._storage.get(index);
+  if (!Array.isArray(tempArrToModify)) {
+    tempArrToModify = [];
+  }
+  for (let i = 0; i < tempArrToModify.length; i++) {
+    if (tempArrToModify[i][0] === k) {
+      tempArrToModify[i][1] = v;
+      return this._storage.set(index, tempArrToModify);
+    }
+  }
+  tempArrToModify.push([k, v]);
+  this._storage.set(index, tempArrToModify);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var arr = this._storage.get(index);
-  if (arr[0] === k) {
-    return arr[1];
+  var tempArrToReview = this._storage.get(index);
+  for (let i = 0; i < tempArrToReview.length; i++) {
+    if (tempArrToReview[i][0] === k) {
+      return tempArrToReview[i][1];
+    }
   }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
+  // lookup value in set?
+  // remove it:
+
 };
 
 
