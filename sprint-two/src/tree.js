9d4a30d3b4e08bdc1e7d299932b8668428aac84e
leerssej <jens.leerssen@gmail.com>
2018-06-15 01:27:43 -0700

    Tree contains logic figured out refactored into es6

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -8,26 +8,56 @@ Tree.prototype.addChild = function(value) {
 };
 
 Tree.prototype.contains = function(target) {
-  // this.children.forEach(element => {
-  //   if (element.value === target) {
-  //     return true;
-  //   } else {
-  //     return element.contains(target);
-  //   }
-  // })
-  var truthy = false;
-  var helper = function (array) {
-    for (var x = 0; x < array.length; x++) {
-      if (array[x].value === target) {
-        return truthy = true;
+  // store result outside internal recursive engine
+  let found = false;
+  // recursive helper function
+  const childChecker = (childrenArr) => {
+    // iterate through array
+    childrenArr.forEach(element => {
+      // base case: check to see that target is in array
+      // if found there, return true
+      if (element.value === target) {
+        return found = true;
       }
-      if (array[x].children.length > -1) {
-        helper(array[x].children);
+      // recursive case: check to see if there are children
+      if (element.children.length > -1) {
+        // if children dig into that list
+        childChecker(element.children);
       }
-    }
+    });
   };
-  helper(this.children);
-  return truthy;
+  childChecker(this.children);
+  return found;
+
+  // // es6 without Annotations
+  // var found = false;
+  // const childChecker = (childrenArr) => {
+  //   childrenArr.forEach((element) => {
+  //     if (element.value === target) {
+  //       return found = true;
+  //     }
+  //     if (element.children.length > -1) {
+  //       childChecker(element.children);
+  //     }
+  //   })
+  // };
+  // childChecker(this.children);
+  // return found;
+
+  // // original
+  // var truthy = false;
+  // var helper = function (array) {
+  //   for (var x = 0; x < array.length; x++) {
+  //     if (array[x].value === target) {
+  //       return truthy = true;
+  //     }
+  //     if (array[x].children.length > -1) {
+  //       helper(array[x].children);
+  //     }
+  //   }
+  // };
+  // helper(this.children);
+  // return truthy;
 };
 
 
